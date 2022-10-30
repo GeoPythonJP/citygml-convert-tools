1baca803d84d4efd48413a00a61d8c5c74532f66
Hiroshi Omata <op.homata@gmail.com>
2022-10-30 23:13:39 +0900

    Add citygml2ply

--- README.md
+++ README.md
@@ -1,7 +1,87 @@
 citygml-convert-tools
 ===
 
-CityGML 変換ツール
+### インストール
+```
+$ git clone --recursive git@github.com:GeoPythonJP/citygml-convert-tools.git
+$ poetry install
+$ poetry shell
+```
 
-* citygml2obj
-  - CityGML -> OBJファイル変換
+### CityGML 変換ツール
+* citygml2ply
+  - CityGML -> PLYファイル変換
+
+### usage 
+```
+$ cd citygml2ply
+$ python main.py -h
+usage: main.py [-h] -to_srid TO_SRID [-lod LOD] filename
+
+citygml-convert-tools
+
+positional arguments:
+  filename              input CityGML filename
+
+optional arguments:
+  -h, --help            show this help message and exit
+  -to_srid TO_SRID, --to_srid TO_SRID
+                        to SRID
+  -lod LOD, --lod LOD   output lod type 0:lod0 1:lod1 2:lod2
+```
+
+実行例
+```
+$ python main.py data/53392633_bldg_6697_2_op.gml --to_srid=6677
+```
+
+出力ディレクトリ: citygml2ply/output
+
+
+### ライブラリ
+
+#### 単調多角形を三角形分割する手法(耳刈り取り法)
+```
+$ git submodule add https://github.com/joshuaskelly/earcut-python.git citygml2ply/contrib/earcutpython
+```
+
+### 参考コード
+* [AcculusSasao/plateaupy](https://github.com/AcculusSasao/plateaupy)
+* [ksasao/PlateauCityGmlSharp](https://github.com/ksasao/PlateauCityGmlSharp/)
+
+### CitGMLビューワー
+* Windows版
+    * [FZKViewer](https://www.iai.kit.edu/1302.php)
+        * [FZKViewer のインストール（Windows 上）](https://www.kkaneko.jp/tools/win/fzkviewer.html)
+* Mac版
+    * [azul](https://github.com/tudelft3d/azul)
+
+### 座標系
+
+#### CityGMLの座標系　緯度/経度:6697-6668
+* 日本測地系2011における経緯度座標系と東京湾平均海面を基準とする標高の複合座標参照系 6697
+* 日本測地系2011における経緯度座標系 6668
+
+#### 日本測地系2011　平面直角座標系:6669-6687
+```
+系	EPSG (JGD2011)	区域
+1	6669	長崎県、鹿児島県の一部
+2	6670	福岡県、佐賀県、熊本県、大分県、宮崎県、鹿児島県の一部
+3	6671	山口県、島根県、広島県
+4	6672	香川県、愛媛県、徳島県、高知県
+5	6673	兵庫県、鳥取県、岡山県
+6	6674	京都府、大阪府、福井県、滋賀県、三重県、奈良県、和歌山県
+7	6675	石川県、富山県、岐阜県、愛知県
+8	6676	新潟県、長野県、山梨県、静岡県
+9	6677	東京都の一部、福島県、栃木県、茨城県、埼玉県、千葉県、群馬県、神祭川県
+10	6678	青森県、秋田県、山形県、岩手県、宮城県
+11	6679	北海道の一部
+12	6680	北海道の一部
+13	6681	北海道の一部
+14	6682	東京都の一部
+15	6683	沖縄県の一部
+16	6684	沖縄県の一部
+17	6685	沖縄県の一部
+18	6686	東京都の一部
+19	6687	東京都の一部
+```
---
+++ citygml2ply/city_gml.py
@@ -0,0 +1,107 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import os
+import numpy as np
+from lxml import etree
+import open3d as o3d
+from pathlib import Path
+
+from building import Building
+
+
+def str2floats(x):
+    return np.array([float(i) for i in x.text.split(' ')])
+
+
+class CityGml:
+    """ core:CityGml """
+
+    def __init__(self, filename, to_srid="6677"):
+        # filename
+        self.filename = filename
+        self.basename = os.path.basename(filename)
+
+        # mesh code
+        basenames = self.mesh_code = self.basename.split('_')
+        self.mesh_code = basenames[0]
+        # 空間参照 ID (SRID)
+        self.from_srid = basenames[2]
+        self.to_srid = to_srid
+
+        # xml tree
+        tree = etree.parse(filename)
+        root = tree.getroot()
+        self.tree = tree
+        self.root = root
+
+        # nsmap
+        nsmap = {}
+        for k, v in root.nsmap.items():
+            if k is not None:
+                nsmap[k] = v
+        self.nsmap = nsmap
+
+        # buildings
+        self.obj_buildings = []
+
+    def lod0(self):
+        nsmap = self.nsmap
+        tree = self.tree
+
+        # scan cityObjectMember
+        buildings = tree.xpath('/core:CityModel/core:cityObjectMember/bldg:Building', namespaces=nsmap)
+        for building in buildings:
+            obj_building = Building(self.from_srid, self.to_srid)
+
+            # bldg:lod0RoofEdge
+            vals = building.xpath('bldg:lod0RoofEdge/gml:MultiSurface/gml:surfaceMember/gml:Polygon/gml:exterior/gml:LinearRing/gml:posList', namespaces=nsmap)
+            polygons = [str2floats(v).reshape((-1,3)) for v in vals]
+            obj_building.create_triangle_meshes(polygons)
+            self.obj_buildings.append(obj_building)
+
+    def lod1(self):
+        nsmap = self.nsmap
+        tree = self.tree
+
+        # scan cityObjectMember
+        buildings = tree.xpath('/core:CityModel/core:cityObjectMember/bldg:Building', namespaces=nsmap)
+        for building in buildings:
+            obj_building = Building(self.from_srid, self.to_srid)
+
+            # bldg:lod1Solid
+            vals = building.xpath('bldg:lod1Solid/gml:Solid/gml:exterior/gml:CompositeSurface/gml:surfaceMember/gml:Polygon/gml:exterior/gml:LinearRing/gml:posList', namespaces=nsmap)
+            polygons = [str2floats(v).reshape((-1, 3)) for v in vals]
+            obj_building.create_triangle_meshes(polygons)
+            self.obj_buildings.append(obj_building)
+
+    def lod2(self):
+        nsmap = self.nsmap
+        tree = self.tree
+
+        # scan cityObjectMember
+        buildings = tree.xpath('/core:CityModel/core:cityObjectMember/bldg:Building', namespaces=nsmap)
+        for building in buildings:
+            obj_building = Building(self.from_srid, self.to_srid)
+
+            # bldg:GroundSurface, bldg:RoofSurface, bldg:RoofSurface
+            polygon_xpaths = ['bldg:boundedBy/bldg:GroundSurface/bldg:lod2MultiSurface/gml:MultiSurface/gml:surfaceMember/gml:Polygon',
+                              'bldg:boundedBy/bldg:RoofSurface/bldg:lod2MultiSurface/gml:MultiSurface/gml:surfaceMember/gml:Polygon',
+                              'bldg:boundedBy/bldg:WallSurface/bldg:lod2MultiSurface/gml:MultiSurface/gml:surfaceMember/gml:Polygon']
+            polygons = []
+            for polygon_xpath in polygon_xpaths:
+                building_xpaths = building.xpath(polygon_xpath, namespaces=nsmap)
+                for building_xpath in building_xpaths:
+                    vals = building_xpath.xpath('gml:exterior/gml:LinearRing/gml:posList', namespaces=nsmap)
+                    surface = [str2floats(v).reshape((-1, 3)) for v in vals]
+                    polygons.extend(surface)
+
+            obj_building.create_triangle_meshes(polygons)
+            self.obj_buildings.append(obj_building)
+
+    def write_ply(self, outputpath):
+        basedir = Path(os.path.dirname(os.path.abspath(__file__)))
+        for index, obj_building in enumerate(self.obj_buildings):
+            triangle_mesh = obj_building.get_triangle_mesh()
+            pathname = os.path.join(basedir, outputpath, f"{self.mesh_code}_{self.to_srid}_{index}.ply")
+            o3d.io.write_triangle_mesh(pathname, triangle_mesh, write_ascii=True)
