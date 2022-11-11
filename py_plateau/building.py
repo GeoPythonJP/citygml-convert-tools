06b48a05459eb03572460f049f6ecfdc3c9cee8c
nokonoko1203 <healtheworld.1203@gmail.com>
2022-11-11 14:16:06 +0900

    コピーするファイルを間違ったので上書き

--- py_plateau/building.py
+++ py_plateau/building.py
@@ -1,44 +1,111 @@
 #!/usr/bin/env python
 # coding: utf-8
 
+import math
+
+import numpy as np
+import open3d as o3d
 import pyproj
+from contrib.earcutpython.earcut.earcut import earcut
 
 
 class Building:
-    """ bldg:Building"""
+    """bldg:Building"""
 
-    def __init__(self, from_srid="6697", to_srid="4326", lonlat_f=False):
+    def __init__(self, from_srid="6697", to_srid="6677"):
         # super().__init__()
         self.polygons = []
+
         self.vertices = []
+        self.triangles = []
+        self.triangle_meshes = []
 
         # pyproj.Transformer.from_crs(<変換元座標系>, <変換先座標系> [, always_xy])
-        self.transformer = pyproj.Transformer.from_crs(f"epsg:{from_srid}", f"epsg:{to_srid}")
-        self.lonlat_f = lonlat_f
+        self.transformer = pyproj.Transformer.from_crs(
+            f"epsg:{from_srid}", f"epsg:{to_srid}"
+        )
 
-        self.properties = None
-
-    def get_properties(self):
-        return self.properties
-
-    def set_properties(self, properties):
-        self.properties = properties
-
-    def get_vertices(self):
-        return self.vertices
+    def get_triangle_mesh(self):
+        return self.triangle_meshes
 
     def transform_coordinate(self, latitude, longitude, height):
         xx, yy, zz = self.transformer.transform(latitude, longitude, height)
-        if self.lonlat_f:
-            return [yy, xx, zz]
-        else:
-            return [xx, yy, zz]
+        return np.array([xx, yy, zz])
 
-    def create_vertices(self, polygons):
+    def create_triangle_meshes(self, polygons):
+        for poly in polygons:
+            transformed_polygon = [self.transform_coordinate(*x) for x in poly]
+            # CityGMLと法線計算時の頂点の取扱順序が異なるため、反転させる
+            transformed_polygon = transformed_polygon[::-1]
+            transformed_polygon = np.array(transformed_polygon)
+
+            normal = self.get_normal(transformed_polygon)[0]
+            poly_2d = np.zeros((transformed_polygon.shape[0], 2))
+            for i, vertex in enumerate(transformed_polygon):
+                xy = self.to_2d(vertex, normal)
+                poly_2d[i] = xy
+
+            vertices_earcut = earcut(np.array(poly_2d, dtype=np.int).flatten(), dim=2)
+
+            if len(vertices_earcut) > 0:
+                vertices_length = len(self.vertices)
+                self.vertices.extend(transformed_polygon)
+                triangles = np.array(vertices_earcut).reshape((-1, 3))
+                triangles_offset = triangles + vertices_length
+                self.triangles.extend(triangles_offset)
+
+        # create triangle mesh by Open3D
+        triangle_meshes = o3d.geometry.TriangleMesh()
+        triangle_meshes.vertices = o3d.utility.Vector3dVector(self.vertices)
+        triangle_meshes.triangles = o3d.utility.Vector3iVector(self.triangles)
+
+        # 法線の取得
+        triangle_meshes.compute_vertex_normals()
+
+        self.triangle_meshes = triangle_meshes
         self.polygons = polygons
-        self.vertices = []
 
-        for plist in polygons:
-            vertices = [self.transform_coordinate(*x) for x in plist]
-            if len(vertices) > 0:
-                self.vertices.append(vertices)
+    # 3つ以上の点を渡して、ポリゴンの法線を求める
+    @staticmethod
+    def get_normal(poly):
+        normal = np.array([0.0, 0.0, 0.0], dtype=np.float64)
+
+        for i, _ in enumerate(poly):
+            next_index = i + 1
+
+            if next_index == len(poly):
+                next_index = 0
+
+            point_1 = poly[i]
+            point_1_x = point_1[0]
+            point_1_y = point_1[1]
+            point_1_z = point_1[2]
+
+            point_2 = poly[next_index]
+            point_2_x = point_2[0]
+            point_2_y = point_2[1]
+            point_2_z = point_2[2]
+
+            normal[0] += (point_1_y - point_2_y) * (point_1_z + point_2_z)
+            normal[1] += (point_1_z - point_2_z) * (point_1_x + point_2_x)
+            normal[2] += (point_1_x - point_2_x) * (point_1_y + point_2_y)
+
+        if (normal == np.array([0.0, 0.0, 0.0])).all():
+            return (normal, False)
+
+        normal = normal / math.sqrt(
+            normal[0] * normal[0] + normal[1] * normal[1] + normal[2] * normal[2]
+        )
+        return (normal, True)
+
+    # 面と法線を渡して、2次元の座標に変換する
+    @staticmethod
+    def to_2d(p, n):
+        x3 = np.array([1.1, 1.1, 1.1])
+
+        if (n == x3).all():
+            x3 += np.array([1, 2, 3])
+        x3 = x3 - np.dot(x3, n) * n
+        x3 /= math.sqrt((x3**2).sum())
+        y3 = np.cross(n, x3)
+        return (np.dot(p, x3), np.dot(p, y3))
--- py_plateau/city_gml.py
+++ py_plateau/city_gml.py
@@ -2,32 +2,29 @@
 # coding: utf-8
 
 import os
-import numpy as np
-import lxml
-from lxml import etree
 from pathlib import Path
-import traceback
 
+import numpy as np
+import open3d as o3d
 from building import Building
-import geopandas as gpd
-from shapely.geometry import Polygon, MultiPolygon
+from lxml import etree
 
 
 def str2floats(x):
-    """ x y z -> [x, y, z] """
-    return np.array([float(i) for i in x.text.split(' ')])
+    """x y z -> [x, y, z]"""
+    return np.array([float(i) for i in x.text.split(" ")])
 
 
 class CityGml:
-    """ core:CityGml """
+    """core:CityGml"""
 
-    def __init__(self, filename, to_srid="4326", lonlat_f=False):
+    def __init__(self, filename, to_srid="6677"):
         # filename
         self.filename = filename
-        self.basename = os.path.basename(filename)
+        self.basename = os.path.splitext(os.path.basename(filename))[0]
 
         # split from basename
-        basenames = self.basename.split('_')
+        basenames = self.basename.split("_")
         # メッシュコード
         self.mesh_code = basenames[0]
         # 地物型 (bldg)
@@ -44,59 +41,25 @@ class CityGml:
 
         # buildings
         self.obj_buildings = []
-        self.lonlat_f = lonlat_f
-
-    def get_bldg_properties(self, building, nsmap):
-        properties = dict()
-        try:
-            # bldg:Building attribute=>gml:id
-            attrib_id = "{%s}id" % nsmap['gml']
-            if attrib_id in building.attrib:
-                properties["id"] = building.attrib[attrib_id]
-
-            # gen:stringAttribute
-            string_attributes = building.xpath('gen:stringAttribute', namespaces=nsmap)
-            for string_attribute in string_attributes:
-                properties[string_attribute.attrib['name']] = string_attribute.getchildren()[0].text
-
-            # bldg:measuredHeight
-            measured_heights = building.xpath('bldg:measuredHeight', namespaces=nsmap)
-            if len(measured_heights) > 0:
-                properties["measured_height_uom"] = measured_heights[0].attrib["uom"]
-                properties["measured_height"] = float(measured_heights[0].text)
-
-            # bldg:address
-            addresses = building.xpath('bldg:address/core:Address/core:xalAddress/xAL:AddressDetails/xAL:Country/xAL:Locality/xAL:LocalityName', namespaces=nsmap)
-            if len(addresses) > 0:
-                properties["address_type"] = addresses[0].attrib["Type"]
-                properties["address"] = addresses[0].text
-            else:
-                properties["address_type"] = ""
-                properties["address"] = ""
-
-        except lxml.etree.XPathEvalError as e:
-            print(e)
-            traceback.print_exc()
-
-        return properties
 
     def lod0(self):
         nsmap = self.root.nsmap
         tree = self.tree
 
         # scan cityObjectMember
-        buildings = tree.xpath('/core:CityModel/core:cityObjectMember/bldg:Building', namespaces=nsmap)
+        buildings = tree.xpath(
+            "/core:CityModel/core:cityObjectMember/bldg:Building", namespaces=nsmap
+        )
         for building in buildings:
-            obj_building = Building(self.from_srid, self.to_srid, self.lonlat_f)
-
-            # set properties
-            properties = self.get_bldg_properties(building, nsmap)
-            obj_building.set_properties(properties)
+            obj_building = Building(self.from_srid, self.to_srid)
 
             # bldg:lod0RoofEdge
-            vals = building.xpath('bldg:lod0RoofEdge/gml:MultiSurface/gml:surfaceMember/gml:Polygon/gml:exterior/gml:LinearRing/gml:posList', namespaces=nsmap)
-            polygons = [str2floats(v).reshape((-1, 3)) for v in vals]
-            obj_building.create_vertices(polygons)
+            faces = building.xpath(
+                "bldg:lod0RoofEdge/gml:MultiSurface/gml:surfaceMember/gml:Polygon/gml:exterior/gml:LinearRing/gml:posList",
+                namespaces=nsmap,
+            )
+            polygons = [str2floats(face_str).reshape((-1, 3)) for face_str in faces]
+            obj_building.create_triangle_meshes(polygons)
             self.obj_buildings.append(obj_building)
 
     def lod1(self):
@@ -104,18 +67,19 @@ class CityGml:
         tree = self.tree
 
         # scan cityObjectMember
-        buildings = tree.xpath('/core:CityModel/core:cityObjectMember/bldg:Building', namespaces=nsmap)
+        buildings = tree.xpath(
+            "/core:CityModel/core:cityObjectMember/bldg:Building", namespaces=nsmap
+        )
         for building in buildings:
-            obj_building = Building(self.from_srid, self.to_srid, self.lonlat_f)
-
-            # set properties
-            properties = self.get_bldg_properties(building, nsmap)
-            obj_building.set_properties(properties)
+            obj_building = Building(self.from_srid, self.to_srid)
 
             # bldg:lod1Solid
-            vals = building.xpath('bldg:lod1Solid/gml:Solid/gml:exterior/gml:CompositeSurface/gml:surfaceMember/gml:Polygon/gml:exterior/gml:LinearRing/gml:posList', namespaces=nsmap)
-            polygons = [str2floats(v).reshape((-1, 3)) for v in vals]
-            obj_building.create_vertices(polygons)
+            faces = building.xpath(
+                "bldg:lod1Solid/gml:Solid/gml:exterior/gml:CompositeSurface/gml:surfaceMember/gml:Polygon/gml:exterior/gml:LinearRing/gml:posList",
+                namespaces=nsmap,
+            )
+            polygons = [str2floats(face_str).reshape((-1, 3)) for face_str in faces]
+            obj_building.create_triangle_meshes(polygons)
             self.obj_buildings.append(obj_building)
 
     def lod2(self):
@@ -123,46 +87,38 @@ class CityGml:
         tree = self.tree
 
         # scan cityObjectMember
-        buildings = tree.xpath('/core:CityModel/core:cityObjectMember/bldg:Building', namespaces=nsmap)
+        buildings = tree.xpath(
+            "/core:CityModel/core:cityObjectMember/bldg:Building", namespaces=nsmap
+        )
         for building in buildings:
-            obj_building = Building(self.from_srid, self.to_srid, self.lonlat_f)
-
-            # set properties
-            properties = self.get_bldg_properties(building, nsmap)
-            obj_building.set_properties(properties)
+            obj_building = Building(self.from_srid, self.to_srid)
 
             # bldg:GroundSurface, bldg:RoofSurface, bldg:RoofSurface
-            polygon_xpaths = ['bldg:boundedBy/bldg:GroundSurface/bldg:lod2MultiSurface/gml:MultiSurface/gml:surfaceMember/gml:Polygon',
-                              'bldg:boundedBy/bldg:RoofSurface/bldg:lod2MultiSurface/gml:MultiSurface/gml:surfaceMember/gml:Polygon',
-                              'bldg:boundedBy/bldg:WallSurface/bldg:lod2MultiSurface/gml:MultiSurface/gml:surfaceMember/gml:Polygon']
+            polygon_xpaths = [
+                "bldg:boundedBy/bldg:GroundSurface/bldg:lod2MultiSurface/gml:MultiSurface/gml:surfaceMember/gml:Polygon",
+                "bldg:boundedBy/bldg:RoofSurface/bldg:lod2MultiSurface/gml:MultiSurface/gml:surfaceMember/gml:Polygon",
+                "bldg:boundedBy/bldg:WallSurface/bldg:lod2MultiSurface/gml:MultiSurface/gml:surfaceMember/gml:Polygon",
+            ]
             vals_list = []
             for polygon_xpath in polygon_xpaths:
                 poslist_xpaths = building.xpath(polygon_xpath, namespaces=nsmap)
                 for poslist_xpath in poslist_xpaths:
-                    vals = poslist_xpath.xpath('gml:exterior/gml:LinearRing/gml:posList', namespaces=nsmap)
+                    vals = poslist_xpath.xpath(
+                        "gml:exterior/gml:LinearRing/gml:posList", namespaces=nsmap
+                    )
                     vals_list.extend(vals)
 
             polygons = [str2floats(v).reshape((-1, 3)) for v in vals_list]
-            obj_building.create_vertices(polygons)
+            obj_building.create_triangle_meshes(polygons)
             self.obj_buildings.append(obj_building)
 
-    def write_geojson(self, output_path):
-        buildings = []
-        for obj_building in self.obj_buildings:
-            # create geometry
-            polygons = [Polygon(row) for row in obj_building.get_vertices()]
-            building_data = obj_building.get_properties()
-            building_data["geometry"] = MultiPolygon(polygons)
-
-            # add properties
-            building_data["filename"] = self.basename
-            building_data["mesh_code"] = self.mesh_code
-            building_data["object"] = self.object_name
-
-            buildings.append(building_data)
-
-        # save GeoJSON by geopandas
-        gdf_data = gpd.GeoDataFrame(buildings, crs=f"EPSG:{self.to_srid}")
+    def write_ply(self, output_path):
         basedir = Path(os.path.dirname(os.path.abspath(__file__)))
-        pathname = os.path.join(basedir, output_path, f"{self.mesh_code}_{self.object_name}_{self.to_srid}.geojson")
-        gdf_data.to_file(pathname, driver="GeoJSON")
+        for index, obj_building in enumerate(self.obj_buildings):
+            triangle_mesh = obj_building.get_triangle_mesh()
+            pathname = os.path.join(
+                basedir,
+                output_path,
+                f"{self.mesh_code}_{self.object_name}_{self.to_srid}_{index:02}.ply",
+            )
+            o3d.io.write_triangle_mesh(pathname, triangle_mesh, write_ascii=True)
